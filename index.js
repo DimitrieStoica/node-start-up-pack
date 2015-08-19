06d30ef25c401683304e4dcd9de5e9cf08bab83f
Alex Stoica <dimitrie.stoica@canopy-cloud.com>
2015-08-20 00:51:10 +0100

    Added home route in a route folder and simple request integration testing

--- index.js
+++ index.js
@@ -2,15 +2,16 @@ var express = require('express');
 var config  = require('./config/config');
 var path    = require('path');
 var app     = express();
+var router  = express.Router();
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.disable('etag');
 
-app.get('/', function(req, res){
-  res.send('Hello there ! \n');
-});
+require('./lib/routes/home')(router,app);
+
+app.use('/', router);
 
 app.use(function(req, res, next) {
   var err = new Error('Not found');
