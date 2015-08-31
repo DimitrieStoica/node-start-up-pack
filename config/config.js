ce1880ae142404881b69a295b766d22333634a35
Alex Stoica <dimitrie.stoica@canopy-cloud.com>
2015-08-31 16:57:56 +0100

    mongo + travis

--- config/config.js
+++ config/config.js
@@ -4,22 +4,27 @@ var PRODUCTION = process.env.NODE_ENV === "production";
 var TEST       = process.env.NODE_ENV === "test";
 
 config.express = {
-  port: process.env.PORT || 3000,
-  ip: process.env.VCAP_APP_HOST || "127.0.0.1"
+  ip:   process.env.VCAP_APP_HOST || "127.0.0.1",
+  port: process.env.PORT || 3000
+};
+
+config.mongodb = {
+  port: process.env.MONGODB_PORT || 27017,
+  host: process.env.MONGODB_HOST || "localhost",
+  db:   process.env.MONGODB_DATABASE || "demo"
 };
 
 if (PRODUCTION) {
-  config.express.ip = process.env.VCAP_APP_HOST || "0.0.0.0";
+  config.express.ip   = process.env.VCAP_APP_HOST || "0.0.0.0";
   config.express.port = process.env.VCAP_APP_PORT || 80;
 };
 
 if (TEST) {
+  config.express.ip   = process.env.VCAP_APP_HOST || "localhost";
   config.express.port = process.env.EXPRESS_PORT || 4657;
-  config.cf.app_url = "http://localhost:" + config.express.port;
-  config.credentials = {
-    cf_username: process.env.CF_USERNAME,
-    cf_password: process.env.CF_PASSWORD
-  };
 };
+
+config.dbURL = "mongodb://" + config.mongodb.host + ":" + config.mongodb.port +
+  "/" + config.mongodb.db + "_" + currentENV;
 
 config.currentENV = currentENV;
--- server.js
+++ server.js
@@ -1,10 +1,18 @@
-var app    = require('./index');
-var config = require('./config/config');
-var bole   = require('bole');
+var app      = require('./index');
+var config   = require('./config/config');
+var bole     = require('bole');
+var mongoose = require('mongoose');
 
 bole.output({level: "debug", stream: process.stdout});
 var log = bole("server");
 log.info({env: process.env.NODE_ENV}, "Server process starting");
+
+mongoose.connect(config.dbURL);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'A connection error has occurred:'));
+db.once('open', function (callback) {
+  log.info("Server connected to: " + config.dbURL);
+});
 
 app.listen(config.express.port, config.express.ip, function(error) {
   if(error) {
